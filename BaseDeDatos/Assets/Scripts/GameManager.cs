2f32dfc9cd6ee9cd62b97b895e0f5172ad06b1f1
Sab4teR2 <sabajmikel@gmail.com>
2022-05-30 17:26:06 +0200

    Clicker cambios

---
+++ BaseDeDatos/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Librerías añadidas
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //declaramos nuestro codigo con la funcion ola para poder acceder a el
+    //Declaramos tambien el texto de nuestros puntos en nuestro caso clicks y el nombre de la persona que juega
+    public GuardadoJsonEncript ola;
+    public Text puntosTXT;
+    public Text nombreTXT;
+    public GameObject rankingGO;
+    int puntosDB;
+
+    public void Update()
+    {
+        
+    }
+
+    //Guardamos los puntos de la data base en nuestro caso clicks, cambiamos el texto de puntos a nuestros clicks y lo enviamos al string
+    //Entramos en el ranking de ranking manager e insertamos los punto mostrados antes
+    public void GuardarPuntosDB()
+    {
+        puntosDB = (int)ola.clicks;
+        puntosTXT.text = ola.clicks.ToString();
+
+        rankingGO.GetComponent<RankingManager>().InsertarPuntos(nombreTXT.text, puntosDB);
+    }
+}
---
+++ BaseDeDatos/Assets/reload.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    //Este void hace que reseteemos la escena para poder probar nuestra base de datos
+
+    public void reloade()
+    {
+       SceneManager.LoadScene(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
